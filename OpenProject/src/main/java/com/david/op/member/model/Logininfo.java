c01272598088e0f43a1f59ca5d26fc27cfd0d5d6
David <gksekdnlt2@naver.com>
2018-10-23 02:02:34 +0900

    2018-10-23 02:02 loginfo 모델수정

--- OpenProject/src/main/java/com/david/op/member/model/Logininfo.java
+++ OpenProject/src/main/java/com/david/op/member/model/Logininfo.java
@@ -5,71 +5,42 @@ public class Logininfo {
 	private String USERNAME;
 	private String USERFILE;
 	private String REGDATE;
-	private String logmsg;
-	private String logid;
 	
 	public Logininfo() {
-		
 	}
 	
-	
-	public Logininfo(String uSERID, String uSERNAME, String uSERFILE, String rEGDATE , String logmsg, String logid) {
-		USERID = uSERID;
-		USERNAME = uSERNAME;
-		USERFILE = uSERFILE;
-		REGDATE = rEGDATE;
-		this.logmsg = logmsg;
-		this.logid = logid;
+	@Override
+	public String toString() {
+		return "Logininfo [USERID=" + USERID + ", USERNAME=" + USERNAME + ", USERFILE=" + USERFILE + ", REGDATE="
+				+ REGDATE + "]";
 	}
-
-
 
 	public String getUSERID() {
 		return USERID;
 	}
-
 	public void setUSERID(String uSERID) {
 		USERID = uSERID;
 	}
-
 	public String getUSERNAME() {
 		return USERNAME;
 	}
-
 	public void setUSERNAME(String uSERNAME) {
 		USERNAME = uSERNAME;
 	}
-
 	public String getUSERFILE() {
 		return USERFILE;
 	}
-
 	public void setUSERFILE(String uSERFILE) {
 		USERFILE = uSERFILE;
 	}
-	
 	public String getREGDATE() {
 		return REGDATE;
 	}
-
 	public void setREGDATE(String rEGDATE) {
 		REGDATE = rEGDATE;
 	}
 
-	public String getLogmsg() {
-		return logmsg;
-	}
-
-	public void setLogmsg(String logmsg) {
-		this.logmsg = logmsg;
-	}
-
-	public String getLogid() {
-		return logid;
-	}
-
-	public void setLogid(String logid) {
-		this.logid = logid;
-	}
 	
+	
+
 }
